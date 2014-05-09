e293bc4bd53bcdd1b1809c6a738b4e2380b72070
Klemen Novak <klemen@kinkcreative.com>
2014-05-09 09:55:13 -0700

    Bower and Grunt.js setup

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// Tasks
+   });
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('');
+
+	// Default task(s).
+	grunt.registerTask('default', ['']);
+ };
