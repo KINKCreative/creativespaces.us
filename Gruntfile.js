bcd637627e80705acb97985ba8585c89641321a0
Klemen Novak <klemen@kinkcreative.com>
2014-05-09 12:09:21 -0700

    Compass init

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,51 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// Tasks
-   });
+		watch: {
+			compass: {
+				files: ['themes/**/scss/*.{scss,sass}'],
+				tasks: ['compass:dev']
+			},
+			js: {
+				files: ['themes/**/js/*.js'],
+				tasks: ['uglify']
+			}
+		},
+		compass: {
+		 	dev: {
+		 		options: {
+		 			sassDir: ['themes/foundation/scss'],
+		 			cssDir: ['themes/foundation/css'],
+		 			environment: 'development'
+		 		}
+		 	},
+			prod: {
+				options: {
+					sassDir: ['themes/foundation/sass'],
+					cssDir: ['themes/foundation/css'],
+					environment: 'production'
+				},
+			}
+		},
+		uglify: {
+			all: {
+				files: {
+					'js/min/app.min.js': [
+						'js/foundation/bower_components/jquery/dist/jquery.min.js', 
+						'js/foundation/bower_components/foundation/js/foundation.min.js',
+						'js/src/main.js'
+					]
+				}
+			},
+		}
+	});
 
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('');
+	// Dependent plugins.
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
 	// Default task(s).
-	grunt.registerTask('default', ['']);
+	grunt.registerTask('default', ['compass:dev' , 'uglify' , 'watch']);
  };
