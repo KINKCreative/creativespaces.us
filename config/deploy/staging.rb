9da7ad4a6426d691afd995222887b2f1ae76a6d9
Klemen Novak <klemen@kinkcreative.com>
2014-05-09 15:09:26 -0700

    Updated staging setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@garabatos.me}
-role :web, %w{deploy@garabatos.me}
-role :db,  %w{deploy@garabatos.me}
+role :app, %w{deploy@creativespaces.us}
+role :web, %w{deploy@creativespaces.us}
+role :db,  %w{deploy@creativespaces.us}
 
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'creativespaces.us', user: 'deploy', roles: %w{web app}, my_property: :my_value
