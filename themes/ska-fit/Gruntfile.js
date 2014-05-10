6adc265757f06dc6da9f79a94d25cd55ccf9087b
Klemen Novak <klemen@kinkcreative.com>
2014-05-10 11:49:58 -0700

    Disable global main site

---
+++ themes/ska-fit/Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		compass: {
+		 	dev: {
+		 		options: {
+		 			sassDir: ['scss'],
+		 			cssDir: ['css'],
+		 			environment: 'development'
+		 		}
+		 	},
+			prod: {
+				options: {
+					sassDir: ['scss'],
+		 			cssDir: ['css'],
+					environment: 'production'
+				},
+			}
+		},
+		watch: {
+			compass: {
+				files: ['scss/*.{scss,sass}'],
+				tasks: ['compass:dev']
+			},
+			js: {
+				files: ['js/*.js'],
+				tasks: ['uglify']
+			}
+		},
+		uglify: {
+			all: {
+				files: {
+					'js/min/app.min.js': [
+						'../../js/foundation/bower_components/jquery/dist/jquery.min.js', 
+						'../../js/foundation/bower_components/foundation/js/foundation.min.js',
+						'js/src/main.js'
+					]
+				}
+			},
+		}
+	});
+
+	// Dependent plugins.
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+	// Default task(s).
+	grunt.registerTask('default', ['compass:dev' , 'uglify' , 'watch']);
+ };
