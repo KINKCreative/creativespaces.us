8405a1f0e3d6e95194df70e60a84c17023621243
Klemen Novak <klemen@kinkcreative.com>
2014-05-12 09:49:46 -0700

    Fixed  bug

--- mysite/code/tasks/SubsiteUsersTask.php
+++ mysite/code/tasks/SubsiteUsersTask.php
@@ -23,7 +23,7 @@ class SubsiteUsersTask extends BuildTask {
                     $newGroup->Subsites()->add($s->ID);
                     $newGroup->Roles()->add(1);
 
-        			echo("Added new group for '".$s->$Title."'.<br/>");
+        			echo("Added new group for '".$s->Title."'.<br/>");
 
                     $newMember = Member::create();
                     $name = implode("",explode(".",$s->getPrimaryDomain()));
