9ce64ba1c62839c841c918adacea02a638176079
Klemen Novak <klemen@kinkcreative.com>
2014-06-20 15:49:57 -0700

    Faq fix

--- themes/pawana/js/src/main.js
+++ themes/pawana/js/src/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     $(".faq h3").each(function() {
         answer = ($(this).next("p").html());
         question = $(this).html();
-        dd = dd + '<dd class="accordion-navigation"><a href="#panel'+count+'"><strong>'+question+'</strog></a><div id="panel'+count+'" class="content">'+answer+'</div></dd>';
+        dd = dd + '<dd class="accordion-navigation"><a href="#panel'+count+'"><strong>'+question+'</strong></a><div id="panel'+count+'" class="content">'+answer+'</div></dd>';
         count++;
     });
     $(".faq").html('<dl class="accordion" data-accordion>'+dd+'</dl>');
